b839b91e9c70da44bba962459e1a3f29916de03e
张业鹏 <hellolittlewhite@gmail.com>
2022-02-07 22:45:58 +0800

    add fortune module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { engine} = require('express-handlebars');
+const fortune = require('./lib/fortune.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -25,8 +26,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', {fortune: randomFortune});
+  res.render('about', {fortune: fortune.getFortune()});
 })
 
 app.use((req, res) => {
